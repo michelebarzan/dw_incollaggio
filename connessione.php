81b42279cd93b2f0d72c88328a8a897d3e0746a0
Michele Barzan <michele@servizioglobale.it>
2021-03-16 16:34:49 +0100

    first commit

---
+++ connessione.php
@@ -0,0 +1,10 @@
+<?php
+	$dw_incollaggio_params_file = fopen("C:\dw_incollaggio_params.json", "r") or die("error");
+	$dw_incollaggio_params=json_decode(fread($dw_incollaggio_params_file,filesize("C:\dw_incollaggio_params.json")), true);
+	fclose($dw_incollaggio_params_file);
+
+	$connectionInfo=array("Database"=>"dw_incollaggio", "UID"=>$dw_incollaggio_params['sql_server_info']['username'], "PWD"=>$dw_incollaggio_params['sql_server_info']['password']);
+	$conn = sqlsrv_connect($dw_incollaggio_params['sql_server_info']['ip'],$connectionInfo);
+	if(!$conn)
+		die("error");
+?>
